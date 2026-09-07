57e8757e4136b0523358b0202fce7b75458928eb
mexikoedi <mexikoedi@outlook.com>
2026-09-08 00:04:47 +0200

    coverage.ts zu coverage-config.ts umbenannt

--- frontend/e2e/coverage-fixtures.ts
+++ frontend/e2e/coverage-fixtures.ts
@@ -1,6 +1,6 @@
 /**
  * Diese Datei enthält die Fixtures für die Code Coverage in den E2E-Tests des Projekts.
  */
-import { e2eCoverage } from './coverage';
+import { e2eCoverage } from './coverage-config';
 
 export const { test, expect } = e2eCoverage;
--- frontend/e2e/coverage-teardown.ts
+++ frontend/e2e/coverage-teardown.ts
@@ -1,6 +1,6 @@
 /**
  * Diese Datei enthält die globale Teardown-Funktion für die Code Coverage in den E2E-Tests des Projekts.
  */
-import { e2eCoverage } from './coverage';
+import { e2eCoverage } from './coverage-config';
 
 export default e2eCoverage.globalTeardown;
